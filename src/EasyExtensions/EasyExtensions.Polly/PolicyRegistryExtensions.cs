c7df64aaaee6acbec9df4b95c2679acf50df7f8d
Artem Mikulich <artem.mikulich@amobee.com>
2020-05-04 15:48:47 +0300

    Version 0.1.1 release

--- src/EasyExtensions/EasyExtensions.Polly/PolicyRegistryExtensions.cs
+++ src/EasyExtensions/EasyExtensions.Polly/PolicyRegistryExtensions.cs
@@ -11,6 +11,22 @@ namespace EasyExtensions.Polly
         public static string RetryPolicyName = "RetryPolicy";
         public static string TimeoutPolicyName = "TimeoutPolicy";
 
+        public static string TimeoutRetryAndWaitPolicy = "TimeoutRetryAndWaitPolicy";
+
+        public static IPolicyRegistry<string> AddTimeoutRetryAndWaitPolicy(this IPolicyRegistry<string> policyRegistry, int retryCount, int timeoutSeconds, string prefix = "")
+        {
+            IAsyncPolicy<HttpResponseMessage> simpleRetryPolicy = 
+                Policy.HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+                    .Or<TimeoutRejectedException>()
+                    .WaitAndRetryAsync(retryCount, r => TimeSpan.FromSeconds(r));
+
+            var timeoutPolicy = Policy.TimeoutAsync(TimeSpan.FromSeconds(timeoutSeconds), TimeoutStrategy.Pessimistic);
+
+            policyRegistry.Add($"{prefix}:{TimeoutRetryAndWaitPolicy}", simpleRetryPolicy.WrapAsync(timeoutPolicy));
+
+            return policyRegistry;
+        }
+
         public static IPolicyRegistry<string> AddRetryPolicy(this IPolicyRegistry<string> policyRegistry, int retryCount, string prefix = "")
         {
             IAsyncPolicy<HttpResponseMessage> simpleRetryPolicy = 
@@ -40,6 +56,13 @@ namespace EasyExtensions.Polly
         public static AsyncTimeoutPolicy GetTimeoutPolicy(this IReadOnlyPolicyRegistry<string> policyRegistry, string prefix = "")
         {
             var policy = policyRegistry.Get<AsyncTimeoutPolicy>($"{prefix}:{TimeoutPolicyName}");
+
+            return policy;
+        }
+
+        public static IAsyncPolicy<HttpResponseMessage> GetTimeoutRetryAndWaitPolicy(this IReadOnlyPolicyRegistry<string> policyRegistry, string prefix = "")
+        {
+            var policy = policyRegistry.Get<IAsyncPolicy<HttpResponseMessage>>($"{prefix}:{RetryPolicyName}");
 
             return policy;
         }
